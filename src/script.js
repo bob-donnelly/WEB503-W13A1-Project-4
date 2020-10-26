3a1bd485f865e24c45b0df17404e2a4e35ee4f8f
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-27 11:50:36 +1300

    added comments from MDN

--- src/script.js
+++ src/script.js
@@ -32,10 +32,15 @@ function addTransaction(e) {
             text: text.value,
             amount: +amount.value,
         }
+        transactions.push(transaction)
     }
 }
 
 // Generate Id
+
+//The Math.floor() function returns the largest integer less than or equal to a given number. - MDN
+
+//The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — which you can then scale to your desired range. The implementation selects the initial seed to the random number generation algorithm; it cannot be chosen or reset by the user. - MDN
 
 function generateId() {
     return Math.floor(Math.random() * 10000000)
