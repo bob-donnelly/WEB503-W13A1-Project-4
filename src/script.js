3611b99683341133f2d8f8b38df1b7ea457485e8
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-27 11:45:53 +1300

    added more javascript thanks Ali

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance')
 
-const money_plus = document.getElementById('money_plus')
+const money_plus = document.getElementById('money-plus')
 
 const money_minus = document.getElementById('money-minus')
 
@@ -12,6 +12,29 @@ const text = document.getElementById('text')
 
 const amount = document.getElementById('amount')
 
+// Lines 16 and 18 sets up local storage for testing //
 const localStorageTransactions = JSON.parse('transactions')
 
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : []
 
+//Add transaction // 
+
+function addTransaction(e) {
+    e.preventDefault()
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add text and amount')
+    } else {
+        const transaction = {
+            id: generateId(),
+            text: text.value,
+            amount: +amount.value,
+        }
+    }
+}
+
+// Generate Id
+
+function generateId() {
+    
+}
