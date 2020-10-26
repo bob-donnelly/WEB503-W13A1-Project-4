67cc8967641884c4284854986154d801622ee39d
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-27 11:26:32 +1300

    added javascript thanks Ali

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
 const balance = document.getElementById('balance')
+
 const money_plus = document.getElementById('money_plus')
+
 const money_minus = document.getElementById('money-minus')
+
 const list = document.getElementById('list')
-const form = document.getElementById
+
+const form = document.getElementById('form')
+
+const text = document.getElementById('text')
+
+const amount = document.getElementById('amount')
+
+const localStorageTransactions = JSON.parse('transactions')
+
+
